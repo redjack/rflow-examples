4cb753553f6ce4937202cd384805a20357376345
Michael L. Artz <mlartz@gmail.com>
2014-05-06 23:49:15 -0400

    Update the rails3 component to store request/response

--- rails3/config/extensions.rb
+++ rails3/config/extensions.rb
@@ -4,13 +4,33 @@ class RFlow::Components::Rails3App < RFlow::Component
   input_port :request_port
   output_port :response_port
 
+  attr_accessor :response_counter
+
+  def configure!(config)
+    @response_counter = 0
+  end
+
   def process_message(input_port, input_port_key, connection, message)
     return unless message.data_type_name == 'RFlow::Message::Data::HTTP::Request'
 
-    response = RFlow::Message.new('RFlow::Message::Data::HTTP::Response')
-    response.provenance = message.provenance
-    response.data.content = "Response to #{message.data.uri} from component #{uuid}\n"
+    RFlow.logger.debug "Received HTTP request"
 
-    response_port.send_message response
+    request = Request.new(:data => message.data.content)
+    request.save!
+
+    RFlow.logger.debug "Response logged '#{request.data}'"
+
+    response = Response.new(:data => "PID #{$$} Response #{response_counter}")
+    response.save!
+
+    RFlow.logger.debug "Responding with '#{response.data}'"
+
+    response_message = RFlow::Message.new('RFlow::Message::Data::HTTP::Response')
+    response_message.provenance = message.provenance
+    response_message.data.content = response.data
+
+    response_port.send_message response_message
+
+    @response_counter += 1
   end
 end
