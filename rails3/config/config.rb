be614ee23ad5fc0eed0a14fe0a66e59f2a045c51
John Stoneham <lyric@lyrically.net>
2014-06-11 21:26:59 -0400

    Add a rails example that uses multiple shards and many-to-many connections, just for testing and example purposes.

--- rails3/config/config.rb
+++ rails3/config/config.rb
@@ -8,11 +8,30 @@ RFlow::Configuration::RubyDSL.configure do |config|
     'listen' => '0.0.0.0',
   }
 
-  config.shard 'rails3', :process => 10 do |shard|
-    shard.component 'rails3_app', 'RFlow::Components::Rails3App'
-  end
+  if true
+    # Handle HTTP messages directly like Rails does
+    config.shard 'rails3', :process => 10 do |shard|
+      shard.component 'rails3_app', 'RFlow::Components::Rails3App'
+    end
 
-  # Server -> Responder -> Server - send the http responses
-  config.connect 'http_server#request_port' => 'rails3_app#request_port'
-  config.connect 'rails3_app#response_port' => 'http_server#response_port'
+    # Server -> Responder -> Server - send the http responses
+    config.connect 'http_server#request_port' => 'rails3_app#request_port'
+    config.connect 'rails3_app#response_port' => 'http_server#response_port'
+  else
+    # A more complicated topology as a many-to-many example
+    config.shard 'envelope', :process => 3 do |shard|
+      shard.component 'remove_envelope', 'RFlow::Components::RemoveEnvelope'
+      shard.component 'add_envelope', 'RFlow::Components::AddEnvelope'
+    end
+
+    config.shard 'raw', :process => 10 do |shard|
+      shard.component 'raw_app', 'RFlow::Components::RawApp'
+    end
+
+    # Server -> RemoveEnvelope -> Responder -> Add Envelope -> Server
+    config.connect 'http_server#request_port' => 'remove_envelope#request_port'
+    config.connect 'remove_envelope#content_port' => 'raw_app#request_port'
+    config.connect 'raw_app#response_port' => 'add_envelope#content_port'
+    config.connect 'add_envelope#response_port' => 'http_server#response_port'
+  end
 end
--- rails3/config/extensions.rb
+++ rails3/config/extensions.rb
@@ -2,6 +2,72 @@ require_relative 'environment'
 
 class RFlow
   module Components
+    # Removes an HTTP request envelope and sends along the content as a raw message.
+    class RemoveEnvelope < Component
+      input_port :request_port
+      output_port :content_port
+
+      def process_message(input_port, input_port_key, connection, message)
+        return unless message.data_type_name == 'RFlow::Message::Data::HTTP::Request'
+        RFlow.logger.debug "Received HTTP request"
+
+        content_port.send_message(
+          RFlow::Message.new('RFlow::Message::Data::Raw').tap do |m|
+            m.provenance = message.provenance
+            m.data.raw = message.data.content
+          end)
+      end
+    end
+
+    # Receives a raw message and sends along the content as an HTTP response.
+    class AddEnvelope < Component
+      input_port :content_port
+      output_port :response_port
+
+      def process_message(input_port, input_port_key, connection, message)
+        return unless message.data_type_name == 'RFlow::Message::Data::Raw'
+
+        response_port.send_message(RFlow::Message.new('RFlow::Message::Data::HTTP::Response').tap do |m|
+          m.provenance = message.provenance
+          m.data.content = message.data.raw
+        end)
+      end
+    end
+
+    # Receives a raw piece of data and acts just as Rails3App does, but responds with raw.
+    class RawApp < Component
+      input_port :request_port
+      output_port :response_port
+
+      attr_accessor :response_counter
+
+      def configure!(config)
+        @response_counter = 0
+      end
+
+      def process_message(input_port, input_port_key, connection, message)
+        return unless message.data_type_name == 'RFlow::Message::Data::Raw'
+
+        Request.new(:data => message.data.raw).tap do |r|
+          RFlow.logger.debug "Response logged '#{r.data}'"
+          r.save!
+        end
+
+        response = Response.new(:data => "PID #{$$} Response #{response_counter}").tap do |r|
+          RFlow.logger.debug "Responding with '#{r.data}'"
+          r.save!
+        end
+
+        response_port.send_message(RFlow::Message.new('RFlow::Message::Data::Raw').tap do |m|
+          m.provenance = message.provenance
+          m.data.raw = response.data
+        end)
+
+        @response_counter += 1
+      end
+    end
+
+    # A Rails-style app that accepts HTTP requests directly and responds with HTTP responses.
     class Rails3App < Component
       input_port :request_port
       output_port :response_port
