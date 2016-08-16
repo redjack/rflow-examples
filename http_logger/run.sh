4081ef718c0179780cb93dd043aae970adc0b018
John Stoneham <lyric@lyrically.net>
2016-08-16 15:54:02 -0400

    Script updates.

--- http_logger/run.sh
+++ http_logger/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+rm -f rflow.*
+rm -f config.sqlite
 bundle exec rflow load -c config.rb
 bundle exec rflow start -d config.sqlite -g rflow-components-http -g rflow-components-file -e extensions.rb 
